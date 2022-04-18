616a200dcd3fb0e8983f14aaee9d5ea5d0ffcfe5
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-04-18 18:27:46 -0300

    Feito area pra inserção da todo list

---
+++ src/App.styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background-color: #E1F4FF;
+`
+
+export const Area = styled.div`
+    margin: 0 auto;
+    padding: 20px 0px;
+    max-width: 900px;
+`
+export const HeaderText = styled.h1`
+    font-size: 30px;
+    text-align: center;
+    color: #24262D;
+    padding: 10px 0;
+`
