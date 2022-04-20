fff919f71b684bb3edfbebd3e1bb4c6a3e49c730
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-04-20 10:13:51 -0300

    fazendo a fucntion para adicionar itens

--- src/components/inputText/index.tsx
+++ src/components/inputText/index.tsx
@@ -1,11 +1,26 @@
 import * as C from './styles'
+import {KeyboardEvent, useState} from 'react'
+
 
 export const InputText = () => {
+
+    const [inputText, setInputText] = useState('')
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+        console.log(event)
+    }
 
     return(
         <C.Container>
             <div className='image'>+</div>
-            <input type="text" />
+            
+            <input 
+                type="text"
+                placeholder='Adicione uma Tarefa'
+                value={inputText}
+                onChange={event => setInputText(event.target.value)}
+                onKeyUp={handleKeyUp}  
+            />
         </C.Container>
     )
 }
